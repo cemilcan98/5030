bfcce7fc84811862c63b26a9965e74ed2d6d8501
cemilcan98 <cemilcan98@gmail.com>
2021-10-13 20:33:52 -0500

    fixing bugs

--- F21/cemilcan98/app.py
+++ F21/cemilcan98/app.py
@@ -1,70 +1,105 @@
-def toLowerCase(i):
-    if(i[1] in ["en", "en-US", "en-IE", "en-Latn"]):
-        y = i[0].lower()
-        return y
+def toLowerCase(word):
 
-    # Checking for the special letters in Turkish and Azeri 
-    if(i[1] == "tr" or i[1] == "az"):
-        y = i[0]
-        y = y.replace('\u011E', '\u011F')
-        y = y.replace('\u00C7', '\u00E7')
-        y = y.replace('\u00D6', '\u00F6')
-        y = y.replace('\u00D3', '\u00FC')
-        y = y.replace('\u015E', '\u015F')  
-        y = y.replace('\u0049', '\u0131')  # I to ı
-        y = y.lower()
+    if(word[1] in ["en", "en-US", "en-IE", "en-Latn", "en-AU", "en-GB", "en-IE", "en-ZA"]):
 
-        return y
+        upperCaseWord = word[0].lower()
+        return upperCaseWord
+
+    # Checking for the special letters in Turkish and Azeri
+
+    if(word[1] == "tr" or word[1] == "az"):
+
+        upperCaseWord = word[0]
+
+        upperCaseWord = upperCaseWord.replace(
+            '\u011E', '\u011F')  # char is Ğ and ğ
+
+        upperCaseWord = upperCaseWord.replace(
+            '\u00C7', '\u00E7')  # char is Ç and ç
+
+        upperCaseWord = upperCaseWord.replace(
+            '\u00D6', '\u00F6')  # char is Ö and ö
+
+        upperCaseWord = upperCaseWord.replace(
+            '\u00D3', '\u00FC')  # char is Ó and ü
+
+        upperCaseWord = upperCaseWord.replace(
+            '\u015E', '\u015F')  # char is Ş and ş
+
+        upperCaseWord = upperCaseWord.replace(
+            '\u0049', '\u0131')  # char is I and ı
+
+        upperCaseWord = upperCaseWord.lower()
+
+        return upperCaseWord
 
     # Checking for the special cases in Irish
-    if(i[1] == "ga" or i[1] == "ga-IE"):
-        y = i[0]
-        first = i[0][0]
-        second = i[0][1]
-        first_letters = ['n', 't']
-        second_letters = ['A', 'E', 'I', 'O', 'U',
-                          '\u00C1', '\u00C9', '\u00CD', '\u00D3', '\u00D3']
-        upper_diff_letters = ['\u00C1', '\u00C9',
-                              '\u00CD', '\u00D3', '\u00DA']
 
-        lower_diff_letters = ['\u00E1', '\u00E9',
-                              '\u00ED', '\u00F3', '\u00FA']
+    if(word[1] == "ga" or word[1] == "ga-IE"):
+
+        upperCaseWord = word[0]
+
+        if(len(word[0] > 1)):
+            first = word[0][0]
+            second = word[0][1]
+        else:
+            first = word[0][0]
+            second = ""
+
+        first_letters = ['n', 't']
+
+        second_letters = ['A', 'E', 'I', 'O', 'U', '\u00C1',
+                          '\u00C9', '\u00CD', '\u00D3', '\u00D3']
 
         if (first in first_letters):
+
             if(second in second_letters):
-                y = y.lower()
-                y = y[:1] + "-" + y[1:]
-                if(second == '\u00C1'):
-                    y[2] = '\u00E1'
-                if(second == '\u00C9'):
-                    y[2] = '\u00E9'
-                if(second == '\u00CD'):
-                    y[2] = '\u00ED'
-                if(second == '\u00DA'):
-                    y[2] = '\u00FA'
-                if(second == '\u00D3'):
-                    y = y[:2] + '\u00F3' + y[3:]
+
+                upperCaseWord = upperCaseWord.lower()
+                upperCaseWord = upperCaseWord[:1] + "-" + upperCaseWord[1:]
+
+                if(second == '\u00C1'):  # char is Á and á
+                    upperCaseWord[2] = '\u00E1'
+
+                if(second == '\u00C9'):  # char is É and é
+                    upperCaseWord[2] = '\u00E9'
+
+                if(second == '\u00CD'):  # char is Í and í
+                    upperCaseWord[2] = '\u00ED'
+
+                if(second == '\u00DA'):  # char is Ú and ú
+                    upperCaseWord[2] = '\u00FA'
+
+                if(second == '\u00D3'):  # char is Ó and ó
+                    upperCaseWord = upperCaseWord[:2] + \
+                        '\u00F3' + upperCaseWord[3:]
+
         else:
-            y = y.lower()
-        return y
+            upperCaseWord = upperCaseWord.lower()
+        return upperCaseWord
 
     # Greek Cases
-    if(i[1] == "el"):
-        y = i[0]
-        if(y[-1] == '\u03A3'):
-            y = y[:-1] + '\u03C2'
-        y = y.lower()
-        return y
 
-    if(i[1] in ["zh", "ja", "th", "zh-Hans"]):
-        y = i[0]
-        return y
+    if(word[1] == "el"):
+        upperCaseWord = word[0]
+        if(upperCaseWord[-1] == '\u03A3'):
+            upperCaseWord = upperCaseWord[:-1] + '\u03C2'
+
+        upperCaseWord = upperCaseWord.lower()
+        return upperCaseWord
+
+    if(word[1] in ["zh", "ja", "th", "zh-Hans"]):
+
+        upperCaseWord = word[0]
+        return upperCaseWord
 
 
 def open_file(file_name):
+
     f = open(file_name, encoding="utf8")
     lines_text = []
     pieces_text = []
+
     for line in f:
         line = line.rstrip('\n')
         lines_text.append(line)
@@ -74,11 +109,17 @@ def open_file(file_name):
 
 
 def close_file(file_name):
+
     f = open(file_name, encoding="utf8")
     f.close()
 
 
-y = open_file('tests.tsv')
-for k in y:
-    if (k[2] != toLowerCase(k)):
-        print("Failed test: ", k)
+textFile = open_file('tests.tsv')
+
+
+for iteration in textFile:
+
+    if (iteration[2] != toLowerCase(iteration)):
+        print("Failed test: ", iteration)
+
+close_file('tests.tsv')
